c836d4bf8aa84d8ddcb429103dc262cc81e5c786
fecet <xiezej@gmail.com>
2023-08-11 20:00:53 +0800

    refactor

--- lua/wildfire/init.lua
+++ lua/wildfire/init.lua
@@ -10,138 +10,124 @@ local M = {}
 
 ---@type table<integer, table<TSNode|nil>>
 local selections = {}
+local nodes_all = {}
 
 local function inner_node(node, buf)
-  local srow, scol, erow, ecol = ts_utils.get_vim_range({ node:range() })
-  return utils.unsurround_coordinates(srow, scol, erow, ecol, buf)
+    local srow, scol, erow, ecol = ts_utils.get_vim_range({ node:range() })
+    return utils.unsurround_coordinates(srow, scol, erow, ecol, buf)
 end
+local function update_selection_by_node(node)
+    local buf = api.nvim_get_current_buf()
+    local node_has_brackets, current_selction = inner_node(node, buf)
+    local local_selections = selections[buf]
+    local last_selection = local_selections[#local_selections]
+    local use_node = utils.range_match(last_selection, current_selction) or not node_has_brackets
+    utils.update_selection(buf, use_node and node or current_selction)
+    if use_node then
+        table.insert(nodes_all[buf], node)
+        table.insert(local_selections, node)
+    else
+        table.insert(local_selections, current_selction)
+    end
+end
+local function init_by_node(node)
+    local buf = api.nvim_get_current_buf()
 
-function M.init_selection()
-  local buf = api.nvim_get_current_buf()
-  local node = ts_utils.get_node_at_cursor()
-  local node_has_brackets, srow, scol, erow, ecol = inner_node(node, buf)
-
-  if not node_has_brackets then
-    selections[buf] = { [1] = node }
-  else
     selections[buf] = {}
-  end
-
-  utils.update_selection(buf, { srow, scol, erow, ecol })
+    nodes_all[buf] = {}
+    -- utils.update_selection(buf, node)
+    update_selection_by_node(node)
+    -- if vim.tbl_isempty(nodes_all[buf]) and not vim.tbl_isempty(selections[buf]) then
+    -- 	nodes_all[buf] = { node }
+    -- end
+end
+function M.init_selection()
+    local node = ts_utils.get_node_at_cursor()
+    init_by_node(node)
 end
 
 -- Get the range of the current visual selection.
 --
 -- The range starts with 1 and the ending is inclusive.
 ---@return integer, integer, integer, integer
-local function visual_selection_range()
-  local _, csrow, cscol, _ = unpack(vim.fn.getpos("'<")) ---@type integer, integer, integer, integer
-  local _, cerow, cecol, _ = unpack(vim.fn.getpos("'>")) ---@type integer, integer, integer, integer
-
-  local start_row, start_col, end_row, end_col ---@type integer, integer, integer, integer
-
-  if csrow < cerow or (csrow == cerow and cscol <= cecol) then
-    start_row = csrow
-    start_col = cscol
-    end_row = cerow
-    end_col = cecol
-  else
-    start_row = cerow
-    start_col = cecol
-    end_row = csrow
-    end_col = cscol
-  end
-
-  return start_row, start_col, end_row, end_col
-end
 
 ---@param get_parent fun(node: TSNode): TSNode|nil
 ---@return fun():nil
 local function select_incremental(get_parent)
-  return function()
-    local buf = api.nvim_get_current_buf()
-    local nodes = selections[buf]
+    return function()
+        local buf = api.nvim_get_current_buf()
+        local nodes = nodes_all[buf]
 
-    local csrow, cscol, cerow, cecol = visual_selection_range()
-    -- Initialize incremental selection with current selection
-    if not nodes or #nodes == 0 then
-      local root = parsers.get_parser():parse()[1]:root()
-      local node = root:named_descendant_for_range(csrow - 1, cscol - 1, cerow - 1, cecol)
-      utils.update_selection(buf, node)
-      if nodes and #nodes > 0 then
-        table.insert(selections[buf], node)
-      else
-        selections[buf] = { [1] = node }
-      end
-      return
-    end
-
-    -- Find a node that changes the current selection.
-    local node = nodes[#nodes] ---@type TSNode
-    while true do
-      local parent = get_parent(node)
-      if not parent or parent == node then
-        -- Keep searching in the main tree
-        -- TODO: we should search on the parent tree of the current node.
-        local root = parsers.get_parser():parse()[1]:root()
-        parent = root:named_descendant_for_range(csrow - 1, cscol - 1, cerow - 1, cecol)
-        if not parent or root == node or parent == node then
-          utils.update_selection(buf, node)
-          return
+        local csrow, cscol, cerow, cecol = utils.visual_selection_range()
+        -- Initialize incremental selection with current selection
+        if not nodes or #nodes == 0 then
+            local root = parsers.get_parser():parse()[1]:root()
+            local node = root:named_descendant_for_range(csrow - 1, cscol - 1, cerow - 1, cecol)
+            update_selection_by_node(node)
+            return
         end
-      end
-      node = parent
-      local nsrow, nscol, nerow, necol = ts_utils.get_vim_range({ node:range() })
-      local node_has_brackets, srow, scol, erow, ecol = utils.unsurround_coordinates(nsrow, nscol, nerow, necol, buf)
 
-      local larger_range = utils.range_larger({ srow, scol, erow, ecol }, { csrow, cscol, cerow, cecol })
+        -- Find a node that changes the current selection.
+        local node = nodes[#nodes] ---@type TSNode
+        while true do
+            local parent = get_parent(node)
+            if not parent or parent == node then
+                -- Keep searching in the main tree
+                -- TODO: we should search on the parent tree of the current node.
+                local root = parsers.get_parser():parse()[1]:root()
+                parent = root:named_descendant_for_range(csrow - 1, cscol - 1, cerow - 1, cecol)
+                if not parent or root == node or parent == node then
+                    utils.update_selection(buf, node)
+                    return
+                end
+            end
+            node = parent
+            local nsrow, nscol, nerow, necol = ts_utils.get_vim_range({ node:range() })
 
-      if larger_range then
-        if not node_has_brackets then
-          table.insert(selections[buf], node)
+            local larger_range = utils.range_larger({ nsrow, nscol, nerow, necol }, { csrow, cscol, cerow, cecol })
+
+            if larger_range then
+                update_selection_by_node(node)
+                return
+            end
         end
-        utils.update_selection(buf, { srow, scol, erow, ecol })
-        return
-      end
-
-      if not larger_range and utils.range_larger({ nsrow, nscol, nerow, necol }, { csrow, cscol, cerow, cecol }) then
-        table.insert(selections[buf], node)
-        utils.update_selection(buf, { nsrow, nscol, nerow, necol })
-        return
-      end
     end
-  end
 end
 
 M.node_incremental = select_incremental(function(node)
-  return node:parent() or node
+    return node:parent() or node
 end)
 
 M.scope_incremental = select_incremental(function(node)
-  local lang = parsers.get_buf_lang()
-  if queries.has_locals(lang) then
-    return locals.containing_scope(node:parent() or node)
-  else
-    return node
-  end
+    local lang = parsers.get_buf_lang()
+    if queries.has_locals(lang) then
+        return locals.containing_scope(node:parent() or node)
+    else
+        return node
+    end
 end)
 
 function M.node_decremental()
-  local buf = api.nvim_get_current_buf()
-  local nodes = selections[buf]
-  if not nodes or #nodes < 2 then
-    return
-  end
+    local buf = api.nvim_get_current_buf()
+    local nodes = nodes_all[buf]
+    if not nodes or #nodes < 2 then
+        return
+    end
 
-  table.remove(selections[buf])
-  local node = nodes[#nodes] ---@type TSNode
-  utils.update_selection(buf, node)
+    local local_selections = selections[buf]
+    table.remove(local_selections)
+    local last_selection = local_selections[#local_selections]
+
+    utils.update_selection(buf, last_selection)
+    if type(last_selection) ~= "table" then
+        table.remove(nodes)
+    end
 end
 
 function M.visual_inner()
-  local buf = api.nvim_get_current_buf()
-  local csrow, cscol, cerow, cecol = visual_selection_range()
-  local _, srow, scol, erow, ecol = utils.unsurround_coordinates(csrow, cscol, cerow, cecol, buf)
-  utils.update_selection(buf, { srow, scol, erow, ecol })
+    local buf = api.nvim_get_current_buf()
+    local csrow, cscol, cerow, cecol = utils.visual_selection_range()
+    local _, srow, scol, erow, ecol = utils.unsurround_coordinates(csrow, cscol, cerow, cecol, buf)
+    utils.update_selection(buf, { srow, scol, erow, ecol })
 end
 return M
--- lua/wildfire/utils.lua
+++ lua/wildfire/utils.lua
@@ -4,108 +4,146 @@ local ts_utils = require("nvim-treesitter.ts_utils")
 local ts = vim.treesitter
 
 local M = {}
+function M.get_range(node_or_range)
+    local start_row, start_col, end_row, end_col ---@type integer, integer, integer, integer
+    if type(node_or_range) == "table" then
+        start_row, start_col, end_row, end_col = unpack(node_or_range)
+    else
+        local buf = api.nvim_get_current_buf()
+        start_row, start_col, end_row, end_col = ts_utils.get_vim_range({ ts.get_node_range(node_or_range) }, buf)
+    end
+    return start_row, start_col, end_row, end_col ---@type integer, integer, integer, integer
+end
 
+function M.visual_selection_range()
+    local _, csrow, cscol, _ = unpack(vim.fn.getpos("'<")) ---@type integer, integer, integer, integer
+    local _, cerow, cecol, _ = unpack(vim.fn.getpos("'>")) ---@type integer, integer, integer, integer
+
+    local start_row, start_col, end_row, end_col ---@type integer, integer, integer, integer
+
+    if csrow < cerow or (csrow == cerow and cscol <= cecol) then
+        start_row = csrow
+        start_col = cscol
+        end_row = cerow
+        end_col = cecol
+    else
+        start_row = cerow
+        start_col = cecol
+        end_row = csrow
+        end_col = cscol
+    end
+
+    return start_row, start_col, end_row, end_col
+end
 function M.range_larger(range1, range2)
-  local srow1, scol1, erow1, ecol1 = unpack(range1)
-  local srow2, scol2, erow2, ecol2 = unpack(range2)
-  if srow1 < srow2 then
-    return true
-  elseif srow1 == srow2 and scol1 < scol2 then
-    return true
-  elseif erow1 > erow2 then
-    return true
-  elseif erow1 == erow2 and ecol1 > ecol2 then
-    return true
-  else
-    return false
-  end
+    local srow1, scol1, erow1, ecol1 = M.get_range(range1)
+    local srow2, scol2, erow2, ecol2 = M.get_range(range2)
+    if srow1 < srow2 then
+        return true
+    elseif srow1 == srow2 and scol1 < scol2 then
+        return true
+    elseif erow1 > erow2 then
+        return true
+    elseif erow1 == erow2 and ecol1 > ecol2 then
+        return true
+    else
+        return false
+    end
+end
+
+function M.range_match(range1, range2)
+    if range1 == nil or range2 == nil then
+        return false
+    end
+
+    local srow1, scol1, erow1, ecol1 = M.get_range(range1)
+    local srow2, scol2, erow2, ecol2 = M.get_range(range2)
+    return srow1 == srow2 and scol1 == scol2 and erow1 == erow2 and ecol1 == ecol2
 end
 
 function M.print_selection(node_or_range)
-  local bufnr = api.nvim_get_current_buf()
-  local lines
-  if type(node_or_range) == "table" then
-    local srow, scol, erow, ecol
-    srow, scol, erow, ecol = unpack(node_or_range)
-    lines = vim.api.nvim_buf_get_text(bufnr, srow - 1, scol - 1, erow - 1, ecol, {})
-  else
-    lines = ts_utils.get_node_text(node_or_range, bufnr)
-  end
-  local node_text = table.concat(lines, "\n")
-  print(node_text)
+    local bufnr = api.nvim_get_current_buf()
+    local lines
+    if type(node_or_range) == "table" then
+        local srow, scol, erow, ecol
+        srow, scol, erow, ecol = unpack(node_or_range)
+        lines = vim.api.nvim_buf_get_text(bufnr, srow - 1, scol - 1, erow - 1, ecol, {})
+    else
+        lines = ts_utils.get_node_text(node_or_range, bufnr)
+    end
+    local node_text = table.concat(lines, "\n")
+    print(node_text)
 end
 
 function M.update_selection(buf, node_or_range, selection_mode)
-  local start_row, start_col, end_row, end_col
+    local start_row, start_col, end_row, end_col
 
-  if type(node_or_range) == "table" then
-    start_row, start_col, end_row, end_col = unpack(node_or_range)
-  else
-    start_row, start_col, end_row, end_col = ts_utils.get_vim_range({ ts.get_node_range(node_or_range) }, buf)
-  end
+    if type(node_or_range) == "table" then
+        start_row, start_col, end_row, end_col = unpack(node_or_range)
+    else
+        start_row, start_col, end_row, end_col = ts_utils.get_vim_range({ ts.get_node_range(node_or_range) }, buf)
+    end
 
-  local v_table = { charwise = "v", linewise = "V", blockwise = "<C-v>" }
-  selection_mode = selection_mode or "charwise"
+    local v_table = { charwise = "v", linewise = "V", blockwise = "<C-v>" }
+    selection_mode = selection_mode or "charwise"
 
-  -- Normalise selection_mode
-  if vim.tbl_contains(vim.tbl_keys(v_table), selection_mode) then
-    selection_mode = v_table[selection_mode]
-  end
+    -- Normalise selection_mode
+    if vim.tbl_contains(vim.tbl_keys(v_table), selection_mode) then
+        selection_mode = v_table[selection_mode]
+    end
 
-  -- enter visual mode if normal or operator-pending (no) mode
-  -- Why? According to https://learnvimscriptthehardway.stevelosh.com/chapters/15.html
-  --   If your operator-pending mapping ends with some text visually selected, Vim will operate on that text.
-  --   Otherwise, Vim will operate on the text between the original cursor position and the new position.
-  local mode = api.nvim_get_mode()
-  if mode.mode ~= selection_mode then
-    -- Call to `nvim_replace_termcodes()` is needed for sending appropriate command to enter blockwise mode
-    selection_mode = vim.api.nvim_replace_termcodes(selection_mode, true, true, true)
-    api.nvim_cmd({ cmd = "normal", bang = true, args = { selection_mode } }, {})
-  end
+    -- enter visual mode if normal or operator-pending (no) mode
+    -- Why? According to https://learnvimscriptthehardway.stevelosh.com/chapters/15.html
+    --   If your operator-pending mapping ends with some text visually selected, Vim will operate on that text.
+    --   Otherwise, Vim will operate on the text between the original cursor position and the new position.
+    local mode = api.nvim_get_mode()
+    if mode.mode ~= selection_mode then
+        -- Call to `nvim_replace_termcodes()` is needed for sending appropriate command to enter blockwise mode
+        selection_mode = vim.api.nvim_replace_termcodes(selection_mode, true, true, true)
+        api.nvim_cmd({ cmd = "normal", bang = true, args = { selection_mode } }, {})
+    end
 
-  api.nvim_win_set_cursor(0, { start_row, start_col - 1 })
-  vim.cmd("normal! o")
-  api.nvim_win_set_cursor(0, { end_row, end_col - 1 })
+    api.nvim_win_set_cursor(0, { start_row, start_col - 1 })
+    vim.cmd("normal! o")
+    api.nvim_win_set_cursor(0, { end_row, end_col - 1 })
 end
 function M.unsurround_coordinates(srow, scol, erow, ecol, buf)
-  -- local lines = vim.split(s, "\n")
-  local lines = vim.api.nvim_buf_get_text(buf, srow - 1, scol - 1, erow - 1, ecol, {})
-  local node_text = table.concat(lines, "\n")
-  local match_brackets = string.match(node_text, "^%b{}$")
-    or string.match(node_text, "^%b()$")
-    or string.match(node_text, "^%b[]$")
-  if match_brackets == nil then
-    return false, srow, scol, erow, ecol
-  end
-  lines[1] = lines[1]:sub(2)
-  local nsrow, nscol = 0, 0
-  for index, line in ipairs(lines) do
-    if line:match("%S") then
-      nsrow = index
-      nscol = line:len() - line:match("^%s*(.*)"):len()
-      break
+    -- local lines = vim.split(s, "\n")
+    local lines = vim.api.nvim_buf_get_text(buf, srow - 1, scol - 1, erow - 1, ecol, {})
+    local node_text = table.concat(lines, "\n")
+    local match_brackets = string.match(node_text, "^%b{}$")
+        or string.match(node_text, "^%b()$")
+        or string.match(node_text, "^%b[]$")
+    if match_brackets == nil then
+        return false, { srow, scol, erow, ecol }
     end
-  end
-
-  lines[#lines] = lines[#lines]:sub(1, -2)
-  local nerow, necol = #lines, 0
-  for index = #lines, 1, -1 do
-    local line = lines[index]
-    if line:match("%S") then
-      nerow = index
-      necol = line:len() - line:match("^(.*%S)%s*$"):len()
-      break
+    lines[1] = lines[1]:sub(2)
+    local nsrow, nscol = 0, 0
+    for index, line in ipairs(lines) do
+        if line:match("%S") then
+            nsrow = index
+            nscol = line:len() - line:match("^%s*(.*)"):len()
+            break
+        end
     end
-  end
 
-  -- print(nsrow, nscol, nerow, necol)
+    lines[#lines] = lines[#lines]:sub(1, -2)
+    local nerow, necol = #lines, 0
+    for index = #lines, 1, -1 do
+        local line = lines[index]
+        if line:match("%S") then
+            nerow = index
+            necol = line:len() - line:match("^(.*%S)%s*$"):len()
+            break
+        end
+    end
 
-  nsrow = srow + nsrow - 1
-  nscol = nsrow == srow and scol + nscol + 1 or nscol + 1
-  -- nerow = erow - nerow + 1
-  nerow = srow + nerow - 1
-  necol = nerow == erow and ecol - necol - 1 or lines[nerow - srow + 1]:len() - necol
+    nsrow = srow + nsrow - 1
+    nscol = nsrow == srow and scol + nscol + 1 or nscol + 1
+    -- nerow = erow - nerow + 1
+    nerow = srow + nerow - 1
+    necol = nerow == erow and ecol - necol - 1 or lines[nerow - srow + 1]:len() - necol
 
-  return true, nsrow, nscol, nerow, necol
+    return true, { nsrow, nscol, nerow, necol }
 end
 return M
